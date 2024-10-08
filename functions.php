00b5ed7cd39db3316d4398766e06e10fd3fbb246
Raymond Baghumian <swordart2050@gmail.com>
2024-10-09 02:37:16 +0330

    Disable jQuery Migrate

--- functions.php
+++ functions.php
@@ -50,3 +50,15 @@ add_action( 'manage_posts_custom_column', 'set_post_view_count_column', 10, 2);
 function get_post_view_count( $post_id ){
     return get_post_meta( $post_id, 'post_view_count', true );
 }
+
+// Disable jQuery Migrate
+
+function remove_jquery_migrate( $scripts ) {
+    if ( ! is_admin() && isset( $scripts->registered['jquery'] ) ) {
+        $script = $scripts->registered['jquery'];
+        if ( $script->deps ) {
+            $script->deps = array_diff( $script->deps, array( 'jquery-migrate' ) );
+        }
+    }
+}
+add_action( 'wp_default_scripts', 'remove_jquery_migrate' );
